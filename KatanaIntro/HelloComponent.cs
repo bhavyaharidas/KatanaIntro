59f36311436ad973897e12032e99149b4046febd
bhavyaharidas <bhavya.haridas93@gmail.com>
2018-12-21 16:01:25 -0500

    Learning

---
+++ KatanaIntro/HelloComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatanaIntro
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+    public class HelloComponent
+    {
+        AppFunc _next;
+        public HelloComponent(AppFunc next)
+        {
+            _next = next;
+        }
+        public Task Invoke(IDictionary<string,object> environment)
+        {
+            var resp = environment["owin.ResponseBody"] as Stream;
+            using (var writer = new StreamWriter(resp))
+            {
+                return writer.WriteAsync("Hello");
+            }
+        }
+    }
+}
---
+++ KatanaIntro/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatanaIntro
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use<HelloComponent>();
+            //app.Run(ctx =>
+            //{
+              //  return ctx.Response.WriteAsync("Hello");
+            //}
+               // );
+        }
+    }
+}
